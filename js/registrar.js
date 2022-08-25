5bbdb06ce9aba4829c6e9cd31555029ff7a0ec0f
martinemoyano <memoyano@gmail.com>
2022-08-25 17:43:29 -0300

    trabajo en registro

--- js/registrar.js
+++ js/registrar.js
@@ -1,37 +1,87 @@
-// document.getElementById('txtNombreUsuario').value="jose";
-// document.getElementById('txtClaveUsuario').value="12345";
-// document.getElementById('txtEmailUsuario').value="jose@jose.com";
+// document.getElementById('NombreUsuario').value="jose";
+// document.getElementById('ClaveUsuario').value="12345";
+// document.getElementById('ClaveUsuario2').value="12345";
+// document.getElementById('EmailUsuario').value="jose@jose.com";
+
 class Subscriptor
 {
      constructor(obj) {
         this.nombre  = obj.nombre;
         this.clave  = obj.clave;
+        this.clave2 = obj.clave2;
         this.email  = obj.email;
+        
     }
 }
 
 
 let ArrayDeSubscriptores=[];
 
-VerificaryCargar();
+// VerificaryCargar();
 
 let botonRegistrar = document.getElementById("btnRegistrar")
 botonRegistrar.onclick = () =>{
 
-    let nombreIngresado = document.getElementById('txtNombreUsuario').value;
-    let claveIngresada = document.getElementById('txtClaveUsuario').value;
-    let emailIngresado = document.getElementById('txtEmailUsuario').value;
-
+    let nombreIngresado = document.getElementById('NombreUsuario').value;
+    let claveIngresada = document.getElementById('ClaveUsuario').value;
+    let claveIngresada2 = document.getElementById('ClaveUsuario2').value;
+    let emailIngresado = document.getElementById('EmailUsuario').value;
+    
     /* validaciones */
-    let objGenerico={nombre : nombreIngresado , clave : claveIngresada , email:emailIngresado}
-    ArrayDeSubscriptores.push(new Subscriptor(objGenerico));
+    const form = document.getElementById('form');
+    const valorNombreIngresado = document.getElementById('NombreUsuario');
+    const valorClaveIngresada = document.getElementById('ClaveUsuario');
+    const valorClaveIngresada2 = document.getElementById('ClaveUsuario2');
+    const ValorEmailIngresado = document.getElementById('EmailUsuario');
 
 
-    Guardar()     
+form.addEventListener('btnRegistrar', (e)=>{
+    e.preventDefault()
+    checkInputs()
+})
+
+function checkInputs(){
+    const valorNombreIngresado = nombreIngresado.value.trim();
+    // const valorClaveIngresada = claveIngresada.value.trim();
+    // const valorClaveIngresada2 = claveIngresada2.value.trim();
+    // const valorEmailIngresado = emailIngresado.value.trim();
+
+    if (valorNombreIngresado === ""){
+
+        usuarioError(nombreIngresado, "El usuario no puede estar vacío")
+    }else {
+
+        usuarioCorrecto()
+
+    }
 }
+
+
+function usuarioError(input, message){
+    const formControl = input.parentElement; 
+    const small = formControl.querySelector('small')   
+    
+    small.innerText = message;//div class .ingreso-nombre
+
+    formControl.className = 'ingreso-nombre error';
+}
+
+// function usuarioCorrecto(input){
+
+// }
+function usuarioCorrecto(){
+// checkInputs()
+let objGenerico={nombre : nombreIngresado , clave : claveIngresada , email:emailIngresado}
+ArrayDeSubscriptores.push(new Subscriptor(objGenerico));
+
+
+Guardar()  
+}
+
 function Guardar()
 {
-    localStorage.setItem("ListadoSubscriptores",JSON.stringify( ArrayDeSubscriptores));
+    localStorage.setItem("ListadoSubscriptores",JSON.stringify(ArrayDeSubscriptores));
+    
 }
 
 function VerificaryCargar()
@@ -54,4 +104,5 @@ function VerificaryCargar()
     }
    
 
+}
 }
